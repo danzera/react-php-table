32f4f3dc7c59eb0ba12d583181059d489c5088ef
Dan Zera <dczera@gmail.com>
2020-05-20 16:13:46 -0700

    add base route/template, include main React component and core-js dep

---
+++ assets/js/components/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Main extends React.Component {
+	render() {
+		return (
+			<div className="main">
+				Main component.
+			</div>
+		);
+	}
+}
+
+export default Main;
