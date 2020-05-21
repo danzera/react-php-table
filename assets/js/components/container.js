74c2675ba6d206cef2e4788c7057399d8c626547
Dan Zera <dczera@gmail.com>
2020-05-21 11:41:33 -0700

    add favorite column to data table

--- assets/js/components/container.js
+++ assets/js/components/container.js
@@ -19,6 +19,7 @@ const Container = ({ data }) => {
 				<table className="ui celled table">
 						<thead>
 							<tr>
+								<th>FAVORITE</th>
 								{headersList}
 							</tr>
 						</thead>
@@ -34,18 +35,37 @@ const Container = ({ data }) => {
 const getHeadersList = (headers) => {
 	return headers.map((text, index) => {
 		// omit id field from table
-		if (text.toLowerCase() != 'id') return <th key={index}>{text}</th>;
+		if (text.toLowerCase() != 'id') {
+			// TODO: make a more elegant solution to determining table display names
+			const textArr = text.toUpperCase().split('_');
+			const displayName = textArr.join(' ');
+			
+			return (
+				<th className="single line" key={index}>
+					{displayName}
+				</th>
+			);
+		}
 	});
 }
 
 const getRowsList = ({ headers, vehicles }) => {
 	return vehicles.map((vehicle, index) => {
 		const tdList = [];
+
+		// add icon according to favorites list
+		tdList.push(getFavoritesIcon(vehicle.vin));
+
 		for (let i = 0; i < headers.length; i++) {
 			const key = headers[i];
 			// omit id field from table
-			if (key.toLowerCase() != 'id')
-				tdList.push(<td key={i}>{vehicle[key]}</td>);
+			if (key.toLowerCase() != 'id') {
+				tdList.push(
+					<td key={i}>
+						{vehicle[key]}
+					</td>
+				);
+			}
 		}
 
 		return (
@@ -54,6 +74,25 @@ const getRowsList = ({ headers, vehicles }) => {
 			</tr>
 		);
 	});
+}
+
+const getFavoritesIcon = (vin) => {
+	// vehicle is a favorite
+	if (0) {
+		return (
+			<td className="favorite-td center aligned" key={vin} data-label="Favorite">
+				<i className="star icon"></i>
+			</td>
+		);
+	}
+	// vehicle is not a favorite
+	else {
+		return (
+			<td className="favorite-td center aligned" key={vin} data-label="Favorite">
+				<i className="star outline icon"></i>
+			</td>
+		);
+	}
 }
 
 export default Container;
